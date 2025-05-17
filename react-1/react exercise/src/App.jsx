13f39d77626cc662ea17a30667f93a0833e9dfad
hadiseh-a <hadisea82@gmail.com>
2025-05-17 12:26:40 +0330

    done react 1

---
+++ react-1/react exercise/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import HellloWorld from './HelloWorld'
+import Card from './Card'
+
+function App() {
+  return (
+    <>
+      {/* <HellloWorld/> */}
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ react-1/react exercise/src/HelloWorld.jsx
@@ -0,0 +1,3 @@
+export default function HellloWorld(){
+    return <p>Hello world!</p>
+}
