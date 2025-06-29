01bb80d04b005a6a5e69e409b771870278560030
hadiseh-a <hadisea82@gmail.com>
2025-06-29 14:50:53 +0330

    edit

--- redux-toolkit-homework/redux-toolkit/src/App.jsx
+++ redux-toolkit-homework/redux-toolkit/src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import JobsList from "./components/jobsList";
+import JobsList from "./components/JobsList";
 
 function App() {
   return (
